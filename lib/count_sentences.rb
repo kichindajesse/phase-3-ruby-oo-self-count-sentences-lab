585452dccfa5e393daac237d47bbe9c9b403a584
kichinda <53686544+kichinda@users.noreply.github.com>
2023-02-11 17:24:36 +0300

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,45 @@ require 'pry'
 class String
 
   def sentence?
+    @sentence = self
+    if @sentence.end_with?(".")
+      true
+    else
+      false
+    end
+
+
     
   end
 
   def question?
+      
+      @question = self
+      if @question.end_with?("?")
+        true
+      else
+        false
+      end
+  
+
 
   end
 
   def exclamation?
+    @exclamation = self
+    if @exclamation.end_with?("!")
+      true
+    else
+      false
+    end
 
   end
 
   def count_sentences
+    @count = self
+    @count = @count.split(/[.!?]/)
+    @count = @count.reject {|string| string.empty?}
+    @count.length
 
   end
 end
